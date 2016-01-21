e1537b4deb64db3d3dc65cbd3f5e535eb010e873
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-21 11:25:05 +0000

    Move the `require_once` for the mandatory Jetpack functionality to the top

--- vip-jetpack.php
+++ vip-jetpack.php
@@ -10,6 +10,11 @@
  */
 
 /**
+ * Enable VIP modules required as part of the platform
+ */
+require_once( __DIR__ . '/jetpack-mandatory.php' );
+
+/**
  * Remove certain modules from the list of those that can be activated
  * Blocks access to certain functionality that isn't compatible with the platform.
  */
@@ -18,11 +23,6 @@ add_filter( 'jetpack_get_available_modules', function( $modules ) {
 
 	return $modules;
 }, 999 );
-
-/**
- * Enable VIP modules required as part of the platform
- */
-require_once( __DIR__ . '/jetpack-mandatory.php' );
 
 /**
  * On VIP Go, we always want to use the Go Photon service, instead of WordPress.com's
