30eae461347789574bae1bf0219a4a19cc09ba28
Nick Daugherty <ndaugherty987@gmail.com>
2016-01-27 10:11:41 -0700

    Add a filter to allow the disabling of the Go Photon service
    Will fallback to regular photon when filtered to `false`

--- vip-jetpack.php
+++ vip-jetpack.php
@@ -28,6 +28,10 @@ add_filter( 'jetpack_get_available_modules', function( $modules ) {
  * On VIP Go, we always want to use the Go Photon service, instead of WordPress.com's
  */
 add_filter( 'jetpack_photon_domain', function( $domain, $image_url ) {
+	if ( ! apply_filters( 'wpcom_vip_enable_go_photon', true ) ) {
+		return;
+	}
+
 	// only apply to photon images
 	if ( preg_match( '|^https?://i[0-9]+.wp.com$|', $domain ) ) {
 		return home_url();
